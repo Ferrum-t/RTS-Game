10cde231ce0f6eb0266a3d063d914283693f46b4
Ferrum-t <138443097+Ferrum-t@users.noreply.github.com>
2026-08-18 00:41:55 +0500

    Restore BaseResource (was corrupted by probe)

--- Scripts/WorldObjects/Resources/BaseResource.gd
+++ Scripts/WorldObjects/Resources/BaseResource.gd
@@ -1,1 +1,21 @@
-x
+extends StaticBody3D
+
+class_name BaseResource
+
+@export var resource_amount: int = 500
+
+
+func _ready() -> void:
+	add_to_group("Resource")
+
+
+## Take up to `amount` from this node. Returns how much was actually taken.
+func harvest(amount: int) -> int:
+	if amount <= 0 or resource_amount <= 0:
+		return 0
+	var taken: int = mini(amount, resource_amount)
+	resource_amount -= taken
+	if resource_amount <= 0:
+		resource_amount = 0
+		queue_free()
+	return taken
