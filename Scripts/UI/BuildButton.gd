a21082db132ae0827a1dc243b43595f0a38ed731
Ferrum-t <138443097+Ferrum-t@users.noreply.github.com>
2026-08-17 21:31:06 +0500

    Build button shows resource cost in label

--- Scripts/UI/BuildButton.gd
+++ Scripts/UI/BuildButton.gd
@@ -5,13 +5,29 @@ class_name BuildButton
 var building_data: BuildingData
 
 
-func setup(data: BuildingData):
-
+func setup(data: BuildingData) -> void:
 	building_data = data
+	if data == null:
+		text = "?"
+		return
 
-	text = data.building_name
+	var cost_parts: PackedStringArray = []
+	if data.wood > 0:
+		cost_parts.append("W:%d" % data.wood)
+	if data.stone > 0:
+		cost_parts.append("S:%d" % data.stone)
+	if data.gold > 0:
+		cost_parts.append("G:%d" % data.gold)
+	if data.food > 0:
+		cost_parts.append("F:%d" % data.food)
+
+	if cost_parts.is_empty():
+		text = data.building_name
+	else:
+		text = "%s (%s)" % [data.building_name, ", ".join(cost_parts)]
 
 
-func _pressed():
-
+func _pressed() -> void:
+	if building_data == null:
+		return
 	ConstructionManager.start_building(building_data)
