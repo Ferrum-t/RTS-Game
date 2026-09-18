f1fa85d479775eb5e0afbfe3928530d66e17b672
vicetasty-afk <vicetasty@gmail.com>
2026-09-18 16:14:05 +0500

    M10.1c: Esc cancels ghost placement

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,14 +1,22 @@
 extends Node3D
 
 
-
 func _ready() -> void:
 	pass
 
-func _process(_delta):
+
+func _process(_delta) -> void:
 	pass
 
-func _unhandled_input(event):
+
+func _unhandled_input(event) -> void:
+	if event is InputEventKey and event.pressed and not event.echo:
+		if event.keycode == KEY_ESCAPE:
+			var cm := get_node_or_null("/root/ConstructionManager")
+			if cm != null and cm.has_method("is_placing") and cm.is_placing():
+				cm.cancel_build_mode()
+				get_viewport().set_input_as_handled()
+				return
 
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
